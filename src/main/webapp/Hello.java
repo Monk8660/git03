434f640e23415ab8c6515d89738a024c24c18af2
Monk8660 <2997878436@qq.com>
2019-09-03 15:44:58 +0800

    提交文件

---
+++ src/main/webapp/Hello.java
@@ -0,0 +1,11 @@
+package main.webapp;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+import sun.applet.Main;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
