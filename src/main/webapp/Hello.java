d2b5042707294d07e92ef0fbd1780a0518fe5af0
Monk8660 <2997878436@qq.com>
2019-09-03 16:20:03 +0800

    hhh

--- src/main/webapp/Hello.java
+++ src/main/webapp/Hello.java
@@ -3,9 +3,9 @@ package main.webapp;
 import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 import sun.applet.Main;
 
-
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("TEST");
     }
 }
